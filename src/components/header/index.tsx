a15c1c92b2074e93171c048a90dc868b56ffc163
pixiu001 <153519193+pixiu001@users.noreply.github.com>
2025-02-21 15:39:38 +0800

    Update index.tsx

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -47,15 +47,8 @@ export function Header() {
           <div className="h-10 w-10 bg-cover" title="logo" style={{ backgroundImage: "url(/icon.svg)" }} />
           <span className="text-2xl font-brand line-height-none!">
             <p>心尘博客</p>
-            <p className="mt--1">
-              <span className="color-primary-6">N</span>
-              <span>ow</span>
-            </p>
           </span>
         </Link>
-        <a target="_blank" href={`${Homepage}/releases/tag/v${Version}`} className="btn text-sm ml-1 font-mono">
-          {`v${Version}`}
-        </a>
       </span>
       <span className="justify-self-center">
         <span className="hidden md:(inline-block)">
