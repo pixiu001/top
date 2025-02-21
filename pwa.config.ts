522c015aa40b73294c24444a5e9155a08ae61b73
pixiu001 <153519193+pixiu001@users.noreply.github.com>
2025-02-21 16:52:11 +0800

    Update pwa.config.ts

--- pwa.config.ts
+++ pwa.config.ts
@@ -6,8 +6,8 @@ const pwaOption: Partial<VitePWAOptions> = {
   includeAssets: ["icon.svg", "apple-touch-icon.png"],
   filename: "swx.js",
   manifest: {
-    name: "NewsNow",
-    short_name: "NewsNow",
+    name: "心尘博客",
+    short_name: "心尘博客",
     description: "Elegant reading of real-time and hottest news",
     theme_color: "#F14D42",
     icons: [
